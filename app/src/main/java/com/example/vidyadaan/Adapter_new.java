5fdd9bacf56e1b6921f8e7a77cbe70a13852d2d8
Vedanty5501 <Vedanty5501@gmail.com>
2023-02-17 13:53:54 +0530

    donor side donation feature added successfull.......

--- app/src/main/java/com/example/vidyadaan/Adapter_new.java
+++ app/src/main/java/com/example/vidyadaan/Adapter_new.java
@@ -1,12 +1,58 @@
 package com.example.vidyadaan;
 
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.FileInputStream;
 import java.util.List;
 
-public class Adapter_new {
+public class Adapter_new extends RecyclerView.Adapter<Adapter_new.viewHolder>{
     private List<requestModel> requestdetails;
 
     public Adapter_new(List<requestModel> requestdetails){this.requestdetails=requestdetails;}
 
+    @NonNull
+    @Override
+    public Adapter_new.viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_donor_needs,parent,false);
+        return new Adapter_new.viewHolder(view);
+    }
 
+    @Override
+    public void onBindViewHolder(@NonNull Adapter_new.viewHolder holder, int position) {
+
+        String ngo_request_id = requestdetails.get(position).getRequestid();
+        String ngo_request_desc = requestdetails.get(position).getRequestdesc();
+
+        holder.setData(ngo_request_id,ngo_request_desc);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return requestdetails.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder{
+
+        private TextView request_id,request_desc;
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            request_id = itemView.findViewById(R.id.requestid);
+            request_desc = itemView.findViewById(R.id.requestdesc);
+        }
+
+        public void setData(String ngo_request_id, String ngo_request_desc) {
+            request_id.setText(ngo_request_id);
+            request_desc.setText(ngo_request_desc);
+        }
+    }
 
 }
--- app/src/main/java/com/example/vidyadaan/ngo_item_available.java
+++ app/src/main/java/com/example/vidyadaan/ngo_item_available.java
@@ -1,30 +1,39 @@
 package com.example.vidyadaan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ngo_item_available extends AppCompatActivity {
 
     TextView requestId,prof_text;
+    FloatingActionButton backbtn;
     RecyclerView recyclerView;
     LinearLayoutManager linearLayoutManager;
     List<requestModel> requestdetails;
     List<requestModel> rd;
-    Adapter adapter;
+    Adapter_new adapter;
     FirebaseDatabase firebaseDatabase;
-    DatabaseReference reference,reference_ngo;
+    DatabaseReference reference,reference_ngo,reference_donor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +41,7 @@ public class ngo_item_available extends AppCompatActivity {
         setContentView(R.layout.activity_ngo_item_available);
         requestId = findViewById(R.id.reqId);
         prof_text = findViewById(R.id.prof_text);
+        backbtn = findViewById(R.id.backbtn);
 
         requestId.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,6 +65,83 @@ public class ngo_item_available extends AppCompatActivity {
         rd = new ArrayList<>();
         firebaseDatabase = FirebaseDatabase.getInstance();
         reference = firebaseDatabase.getReference("Donor_Post");
-        reference_ngo = firebaseDatabase.getReference("Donors");
+        reference_ngo = firebaseDatabase.getReference("NGOs");
+        reference_donor = firebaseDatabase.getReference("Donors");
+        try {
+            String filename = "Userdetail.txt";
+            FileInputStream fin = openFileInput(filename);
+            int a, n = 0;
+            StringBuilder temp = new StringBuilder();
+            while ((a = fin.read()) != -1) {
+                if (n < 2) {
+                    n++;
+                    continue;
+                }
+                temp.append((char) a);
+            }
+            String user = temp.toString();
+            reference_ngo.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    String got_pin=snapshot.child(user).child("pin").getValue().toString();
+                    Query query_1 = reference.orderByChild("pin").equalTo(got_pin);
+                    query_1.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snap) {
+                            if(snap.exists()){
+                               reference_donor.addListenerForSingleValueEvent(new ValueEventListener() {
+                                   @Override
+                                   public void onDataChange(@NonNull DataSnapshot s) {
+                                       for (DataSnapshot data: snap.getChildren()) {
+                                           String rid = data.child("description").getValue().toString();
+                                           String d_id = data.child("id").getValue().toString();
+                                           String contact = s.child(d_id).child("phone").getValue().toString();
+                                           rd.add(new requestModel(rid,contact));
+                                       }
+                                       Log.v("tag1",""+rd);
+                                       initData(rd);
+                                   }
+
+                                   @Override
+                                   public void onCancelled(@NonNull DatabaseError error) {
+
+                                   }
+                               });
+
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+        catch(Exception e){
+
+        }
+    }
+    private void initData(List<requestModel> rd) {
+        requestdetails = new ArrayList<>();
+        requestdetails = rd;
+        Log.v("taggy",""+rd);
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        recyclerView = findViewById(R.id.recyclerview);
+        linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        adapter = new Adapter_new(requestdetails);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 }
